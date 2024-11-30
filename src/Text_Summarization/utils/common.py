9c3e8f6027a1a03349f30298e3daa46ba94c40fb
kunalshelke90 <shelkekunal90@gmail.com>
2024-11-30 23:38:39 +0530

    Utils  completed

--- src/Text_Summarization/utils/common.py
+++ src/Text_Summarization/utils/common.py
@@ -32,7 +32,6 @@ def read_yaml(path_to_yaml: Path) -> ConfigBox:
 @ensure_annotations
 def create_directories(path_to_directories: list, verbose=True):
     """create list of directories
-
     Args:
         path_to_directories (list): list of path of directories
         ignore_log (bool, optional): ignore if multiple dirs is to be created. Defaults to False.
@@ -46,10 +45,8 @@ def create_directories(path_to_directories: list, verbose=True):
 @ensure_annotations
 def get_size(path: Path) -> str:
     """get size in KB
-
     Args:
         path (Path): path of the file
-
     Returns:
         str: size in KB
     """
